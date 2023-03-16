93cdce7f5144d843cad1cf46f98284ad4b3f5548
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2023-03-16 12:24:02 +0600

    this is the ticket app
    ticket app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TicketFlutterApp
+#Project Description: The main goal was to design a complex android/ios ticket booking app 
+#Problems face: github repo records vanished after every commit
+#Database implementation from scratch was not possible as there was no resources available to me to from anywhere regarding this build
+#github history screenshot- https://drive.google.com/drive/folders/16JoL56nCtaQnFbRiIbVUswcOWzKiciZM?usp=sharing
---
+++ lib/db/ticket_list.dart
@@ -0,0 +1,30 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code':"SYL",
+      'name':"Sylhet"
+    },
+    'to': {
+      'code':"DHK",
+      'name':"Dhaka"
+    },
+    'flying_time': '4H 20M',
+    'date': "1 MAY",
+    'departure_time':"08:00 AM",
+    "number":23
+  },
+  {
+    'from': {
+      'code':"DK",
+      'name':"Dhaka"
+    },
+    'to': {
+      'code':"SYL",
+      'name':"Sylhet"
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time':"09:00 PM",
+    "number":45
+  },
+];
